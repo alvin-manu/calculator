4eca2aef312f341f0051b776003c7b33859421fa
Alvin00111 <wmawma905@gmail.com>
2026-04-23 10:40:30 +0530

    added js functionality for calculating , deleting, clearing and adding input from buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 const changeThemeBtn = document.getElementById("changeThemeBtn");
 const themeText = document.getElementById("themeText");
 const themeIcon = document.getElementById("themeIcon");
+const inputContainer = document.getElementById("inputContainer")
+const calculations = document.getElementById("calculations")
 
 changeThemeBtn.addEventListener("click", () => {
     document.body.classList.toggle("darkMode")
@@ -15,6 +17,35 @@ changeThemeBtn.addEventListener("click", () => {
         themeIcon.classList.remove("fa-sun");
         themeIcon.classList.add("fa-moon");
     }
-
-
 })
+
+const addInput = (val) => {
+    if (val === '.') {
+        const parts = inputContainer.value.split(/[\+\-\*\/]/);
+
+        const currentNumber = parts[parts.length - 1];
+
+        if (currentNumber.includes('.')) return;
+    }
+
+    inputContainer.value += val
+}
+
+const clearAll = () => {
+    inputContainer.value = "";
+    calculations.textContent = "";
+};
+
+const deleteLast = () => {
+    inputContainer.value = inputContainer.value.slice(0, -1);
+};
+
+const calculate = () => {
+    try {
+        calculations.textContent = inputContainer.value;
+        inputContainer.value = eval(inputContainer.value);
+    } catch (err) {
+        inputContainer.value = "NaN";
+    }
+};
+
